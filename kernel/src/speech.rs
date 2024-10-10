165bfada4650bf26e39d0786603a029fff695948
luster <luster@flowersky.love>
2024-10-10 20:26:47 +0800

    new thread server no await,  / route

--- kernel/src/speech.rs
+++ kernel/src/speech.rs
@@ -38,8 +38,12 @@ impl SpeechVec {
         self.queue.iter().cloned().collect() // 返回队列中的元素作为向量
     }
 }
+    async fn get_uuid() -> HttpResponse {
+    HttpResponse::Ok().body("hello world".to_string())
+}
 
-pub    async fn run_server() {
+
+pub    fn run_server() {
         let handle = tokio::spawn(async {
             let _ = serve(); 
         });
@@ -48,46 +52,12 @@ pub    async fn run_server() {
 
 
 
-
-// #[pyclass]
-pub struct SpeechQueue {
-    // #[pyo3(get, set)]
-    queue: VecDeque<String>,
-    // #[pyo3(get, set)]
-    max_size: usize, // 最大队列长度
-}
-
-// #[pymethods]
-impl SpeechQueue {
-    // 创建一个新的FixedQueue，传入最大长度
-    // #[new]
-    fn new(max_size: usize) -> Self {
-        SpeechQueue {
-            queue: VecDeque::new(),
-            max_size,
-        }
-    }
-
-    // 向队列中添加元素
-    fn push(&mut self, element: String) {
-        if self.queue.len() == self.max_size {
-            self.queue.pop_front(); // 如果队列已满，移除最前面的元素
-        }
-        self.queue.push_back(element); // 添加新元素到队列末尾
-    }
-
-    // 获取队列中的所有元素
-    fn get_all(&self) -> Vec<String> {
-        self.queue.iter().cloned().collect() // 返回队列中的元素作为向量
-    }
-}
-
-
 #[actix_web::main]
 async fn serve() -> std::io::Result<()> {
     // 启动 HTTP 服务器
     HttpServer::new(|| {
         App::new()
+        .route("/", web::get().to(get_uuid))
     })
     .bind(("127.0.0.1", 6838))? 
     .run()
@@ -115,26 +85,9 @@ pub fn speech(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
 
 #[cfg(test)]
 mod test{
-    #[test]
-        fn test_speech_queue() {
-            use super::*;
-    let mut queue = SpeechQueue::new(3); // 队列最大长度为3
 
-    // 添加元素
-    queue.push("element1".to_string());
-    queue.push("element2".to_string());
-    queue.push("element3".to_string());
-
-    println!("Queue after 3 pushes: {:?}", queue.get_all());
-
-    // 再次添加元素，超过长度时会自动移除前端元素
-    queue.push("element4".to_string());
-
-    println!("Queue after pushing element4: {:?}", queue.get_all());
-}
-
-#[test]
-        fn test_speech_vec() {
+#[tokio::test]
+        async fn test_speech_vec() {
             use super::*;
     let mut queue = SpeechVec::new(3); // 队列最大长度为3
 
